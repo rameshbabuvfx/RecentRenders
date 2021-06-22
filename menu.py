8fae610f0f8f7a2bfaa7f1d94e80a94126f36b37
rameshkanna555 <rameshkanna95@gmail.com>
2021-06-22 19:23:24 +0530

    working on drag events

--- menu.py
+++ menu.py
@@ -1,4 +1,8 @@
 import nukescripts
-from recentRenders import DisplayRenders
+import recentRenders
 
-nukescripts.registerWidgetAsPanel('DisplayRenders', "RecentRenders", 'uk.co.thefoundry.DisplayRenders', True)
+# nukescripts.registerWidgetAsPanel('DisplayRenders', "RecentRenders", 'uk.co.thefoundry.DisplayRenders', True)
+
+menu = nuke.toolbar('Nodes')
+render_menu = menu.addMenu('Recent_Renders')
+render_menu.addCommand('Recent_Renders', 'recentRenders.run()')
--- recentRenders.py
+++ recentRenders.py
@@ -10,9 +10,9 @@ from thumbnails import *
 class TableWidget(QTableWidget):
     def __init__(self):
         super(TableWidget, self).__init__()
-        self.resize(500, 600)
         self.setDragEnabled(True)
         self.setAcceptDrops(True)
+        self.setStyleSheet("QTableWidget{background-color: transparent}")
 
     def mouseMoveEvent(self, event):
         current_value = self.currentItem()
@@ -30,28 +30,52 @@ class TableWidget(QTableWidget):
 class DisplayRenders(QWidget):
     def __init__(self):
         super(DisplayRenders, self).__init__()
-        self.resize(800, 600)
+        self.resize(500, 600)
+
+
+        self.animation = QPropertyAnimation(self, b'geometry')
+        self.animation.setDuration(250)
+        self.animation.setStartValue(QRect(700,300,600,5))
+        self.animation.setEndValue(QRect(700,300,500,600))
+        self.animation.setEasingCurve(QEasingCurve.OutCurve)
+        self.animation.start()
+
+        self.setWindowFlags(Qt.FramelessWindowHint)
+        self.setAttribute(Qt.WA_NoSystemBackground)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        self.row_height = 110
+        self.column_width = 110
         self.gblayout = QGridLayout()
         self.table_widget = TableWidget()
         self.gblayout.addWidget(self.table_widget)
         self.setLayout(self.gblayout)
-
         self.table_widget.setRowCount(10)
         self.table_widget.setColumnCount(4)
-        self.table_widget.setRowHeight(1,160)
-        self.table_widget.setColumnWidth(1,205)
-        self.table_widget.setIconSize(QSize(100,100))
+        self.table_widget.setRowHeight(1,self.row_height)
+        self.table_widget.setColumnWidth(1,self.column_width)
+        self.table_widget.setShowGrid(False)
+        self.table_widget.horizontalHeader().hide()
+        self.table_widget.verticalHeader().hide()
         self.table_widget.setSelectionMode(QAbstractItemView.SingleSelection)
         self.table_widget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
+
+
+
 
         item = QTableWidgetItem(r"F:\NukePython\RecentRenders\thumbnails\blackadam.jpg")
         self.table_widget.setItem(1, 1, item)
         label = QLabel()
         pixmap = QPixmap(r"F:\NukePython\RecentRenders\thumbnails\blackadam.jpg")
-        label.setPixmap(pixmap.scaled(205,160, Qt.KeepAspectRatio, Qt.SmoothTransformation))
+        label.setPixmap(pixmap.scaled(self.column_width, self.row_height, Qt.KeepAspectRatio, Qt.SmoothTransformation))
         label.setScaledContents(True)
         label.resize(10,10)
         self.table_widget.setCellWidget(1, 1, label)
+
+
+def run():
+    run.obj = DisplayRenders()
+    run.obj.show()
 
 
 if __name__ == '__main__':
